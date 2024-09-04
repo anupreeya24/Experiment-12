47299513d7f8cbfb6abd21f3cd696e43fde210a2
anupreeya24 <anupreeyawo@gmail.com>
2024-09-04 11:30:40 +0530

    Create callingconstructor.cpp

---
+++ callingconstructor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class date {
+    int d;
+    int m;
+    int y;
+    
+    public:
+    date()
+    {
+        cout<<"Constructor called"<<endl;
+        d=4;
+        m=9;
+        y=2024;
+    }
+    
+    void output(){
+        cout<<d<<"/";
+        cout<<m<<"/";
+        cout<<y;
+    }
+};
+
+int main() {
+    date today;
+    today.output();
+    return 0;
+}
+
+/* OUTPUT
+Constructor called
+4/9/2024 */
